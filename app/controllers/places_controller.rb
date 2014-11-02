58d233bd4e5f5bda5d89d2a5e5c3cdc77667bea4
Jose Moreno <jose.moreno@reyesic.net>
2014-11-02 16:37:25 +0000

    Show latest comment on homepage

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
     ]
     
     def index
-        @places = Place.all.paginate(page: params[:page], per_page: 4)
+        @places = Place.all.paginate(page: params[:page], per_page: 1)
     end
     
     def new
