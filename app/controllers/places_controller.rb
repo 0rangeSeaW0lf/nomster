14097f29b41967a2ba782b0d0389b0fce18f3351
Jose Moreno <jose.moreno@reyesic.net>
2014-11-05 20:52:14 +0000

    Adjust place controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
     ]
     
     def index
-        @places = Place.all.paginate(page: params[:page], per_page: 1)
+        @places = Place.all.paginate(page: params[:page], per_page: 5)
     end
     
     def new
